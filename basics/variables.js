0d7ca6994f17bd5ce06d7aaaf859b3a676ab7f58
suyogzz78 <sakurachhetri97@gmail.com>
2025-04-29 13:59:18 +0545

    basics of js

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+  const studentid= "224";//constants are immutable
+  let studentname= "suman"//variables are mutable
+  let studentemail= "sutman@gmail.com"
+  studentroll= "21"//not declared with let or const, so it is mutable
+
+
+  studentemail= "spidy@gmail.com"
+  studentroll= "22"
+  console.log(studentid, studentemail, studentroll); 
