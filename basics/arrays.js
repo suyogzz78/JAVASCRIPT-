6d220f287eecba3ff01c19d3b6edf1db794ef167
suyogzz78 <sakurachhetri97@gmail.com>
2025-07-15 20:50:07 +0545

    dom manipulation

--- basics/arrays.js
+++ basics/arrays.js
@@ -10,10 +10,13 @@ myarr.unshift(9);
 console.log(myarr);
 myarr.join();// Convert array to string
 
-const ar =marr2.slice(0, 2);
+const ar = marr2.slice(0, 2);//slice method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array.
 console.log(ar);
 console.log(marr2);
 
-const ar1 =marr2.splice(0, 2);
+const ar1 = marr2.splice(0, 2);//splice method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. It returns an array containing the deleted elements.
 console.log(ar1);
 console.log(marr2);
+
+
+ 
--- basics/variables.js
+++ basics/variables.js
@@ -9,3 +9,11 @@
   console.log(studentid, studentemail, studentroll); 
 
   /* we dont use var in the js because it has issue in block scope*/
+
+  //palindrome in js
+  const str = "madam";
+  const isPalindrome = (str) => {
+    const reversestr= str.split("").reverse().join("");//here we are splitting the string into an array, reversing it, and then joining it back to a string
+    return reversestr === str;
+  }
+  console.log(isPalindrome(str)); // true
